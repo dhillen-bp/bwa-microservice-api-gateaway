a14920e408c1c9e372270c48305677c2b9dc7b96
dhillenbp179 <dhillen.mutu@gmail.com>
2023-10-25 21:03:20 +0700

    create api post-upload image

--- service-media/config/config.js
+++ service-media/config/config.js
@@ -1,8 +1,8 @@
-require("dotenv").config;
+require("dotenv").config();
 
 const { DB_USERNAME, DB_PASSWORD, DB_NAME, DB_HOSTNAME } = process.env;
 
-module.export = {
+module.exports = {
   development: {
     username: DB_USERNAME,
     password: DB_PASSWORD,
