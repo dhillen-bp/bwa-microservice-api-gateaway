e43f6c6b0e72937a5fefa752f01eed1a91e130de
dhillenbp179 <dhillen.mutu@gmail.com>
2023-10-29 06:28:47 +0700

    integration api my course-service course

--- routes/imageCourses.js
+++ routes/imageCourses.js
@@ -3,9 +3,6 @@ const router = express.Router();
 const imageCoursesHandler = require("./handler/image-courses");
 
 router.post("/", imageCoursesHandler.create);
-router.get("/", imageCoursesHandler.getAll);
-router.get("/:id", imageCoursesHandler.get);
-router.put("/:id", imageCoursesHandler.update);
 router.delete("/:id", imageCoursesHandler.destroy);
 
 module.exports = router;
