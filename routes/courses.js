6eceb9f6497fdd59bfd97029f040d48d1854b5ac
dhillenbp179 <dhillen.mutu@gmail.com>
2023-10-31 11:00:19 +0700

    add role & permission

--- routes/courses.js
+++ routes/courses.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const coursesHandler = require("./handler/courses");
+const verifyToken = require("../middlewares/verifyToken");
+const can = require("../middlewares/permission");
 
-router.post("/", coursesHandler.create);
 router.get("/", coursesHandler.getAll);
 router.get("/:id", coursesHandler.get);
-router.put("/:id", coursesHandler.update);
-router.delete("/:id", coursesHandler.destroy);
+
+router.post("/", verifyToken, can("admin"), coursesHandler.create);
+router.put("/:id", verifyToken, can("admin"), coursesHandler.update);
+router.delete("/:id", verifyToken, can("admin"), coursesHandler.destroy);
 
 module.exports = router;
